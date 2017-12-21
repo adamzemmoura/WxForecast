d83d489ae34f9894e166f95be3e067096c234c73
Adam Zemmoura <adamzemmoura@gmail.com>
2017-12-22 00:35:21 +0300

    Added networking code to connect to darksky API. Added AlertDialogFragment class.

---
+++ app/src/main/java/com/adamzemmoura/wxforecast/AlertDialogFragment.java
@@ -0,0 +1,39 @@
+package com.adamzemmoura.wxforecast;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+public class AlertDialogFragment extends DialogFragment {
+
+    private AlertType type = AlertType.Generic_Error;
+
+    enum AlertType {
+        Generic_Error, No_Network_Error
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setPositiveButton(R.string.error_okay_button_text, null);
+
+        switch (type) {
+            case No_Network_Error:
+                builder.setMessage(R.string.network_unavailable_message);
+                break;
+            default:
+                builder.setMessage(R.string.error_message);
+        }
+
+        return builder.create();
+    }
+
+    public void setType(AlertType type) {
+        this.type = type;
+    }
+}
--- app/src/main/java/com/adamzemmoura/wxforecast/MainActivity.java
+++ app/src/main/java/com/adamzemmoura/wxforecast/MainActivity.java
@@ -1,13 +1,89 @@
 package com.adamzemmoura.wxforecast;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = getResources().getText(R.string.api_key).toString();
+        String latitude = "37.8267";
+        String longitude = "-122.4233";
+
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+        Log.d(TAG, forecastUrl);
+        Log.d(TAG, "TEST");
+
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError(null);
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught : ", e);
+                    }
+                }
+            });
+        } else {
+            alertUserAboutError(AlertDialogFragment.AlertType.No_Network_Error);
+        }
+
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private void alertUserAboutError(AlertDialogFragment.AlertType type) {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        if (type != null) {
+            dialog.setType(type);
+        }
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
