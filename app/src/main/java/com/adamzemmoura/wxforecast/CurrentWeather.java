4a2034e1ef23a23dc0b9cb30d7bff6200db1803f
Adam Zemmoura <adamzemmoura@gmail.com>
2017-12-22 03:25:33 +0300

    Added getIconId() function to CurrentWeather to fetch correct icon

--- app/src/main/java/com/adamzemmoura/wxforecast/CurrentWeather.java
+++ app/src/main/java/com/adamzemmoura/wxforecast/CurrentWeather.java
@@ -31,6 +31,39 @@ public class CurrentWeather {
         this.icon = icon;
     }
 
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+
+        switch (icon) {
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+        }
+
+        return iconId;
+    }
+
     public long getTime() {
         return time;
     }
